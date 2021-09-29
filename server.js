bf75eeecaea732195c6fe9ee85ef1c738a64351b
cqrlosdiqz <carlosdf75@gmail.com>
2021-09-29 18:18:31 +0200

    switch statement

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const server = http.createServer((req, res) => {
       res.writeHead(200, {
         'Content-Type': 'text/plain; charset=utf-8',
       });
-      res.write(hour);
+      res.write(hour.toString());
       res.end();
       break;
     default:
